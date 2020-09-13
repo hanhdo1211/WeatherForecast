286f654b4aab02b277379f570d880bc957fe0bbc
HanhDV <hanhdv@vng.com.vn>
2020-09-13 14:16:06 +0700

    [App] Setup WorkSpace

---
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by HanhDV on 9/13/20.
+//  Copyright © 2020 LAP12005. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
